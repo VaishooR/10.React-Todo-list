21c729792c4cf1a4833a5020a5c628ce0104ee8e
VaishooR <96234429+VaishooR@users.noreply.github.com>
2022-08-22 13:37:47 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import './style.css';
 import { useState } from 'react';
 
 export default function App() {
+  
   // input
   const [newtask, setnewtask] = useState('');
   let handleinput = (e) => {
     setnewtask(e.target.value);
   };
 
+  
   // todo-list
   const [todolist, settodolist] = useState([]);
   let handleclick = () => {
@@ -21,12 +23,14 @@ export default function App() {
   };
   console.log(todolist);
 
+  
   // delete function
   let handledelete = (id) => {
     let newlist = todolist.filter((to) => (to.id === id ? false : true));
     settodolist(newlist);
   };
 
+  
   // done function
   let handledone = (id) => {
     let donelist = todolist.map((to) =>
@@ -35,38 +39,32 @@ export default function App() {
     settodolist(donelist);
   };
 
+  
+  
   return (
     <div id="todolist">
+      
       <input type="text" onChange={handleinput} />
       <button onClick={handleclick}>Add task</button>
+      
       <ul>
         {todolist.map((todo) => {
           return (
             <li>
-              <span
-                style={{
-                  textDecoration: todo.completed ? 'line-through' : 'none',
-                }}
-              >
-                {todo.taskname}
-              </span>
-              &nbsp;&nbsp;
-              <button id="del" onClick={() => handledelete(todo.id)}>
-                Delete
-              </button>
-              &nbsp;
-              <button
-                id="done"
-                onClick={() => {
-                  handledone(todo.id);
-                }}
-              >
-                Done
-              </button>
+                <span style={{textDecoration: todo.completed ? 'line-through' : 'none',}}>
+                    {todo.taskname}
+                </span>&nbsp;&nbsp;
+                <button id="del" onClick={() => handledelete(todo.id)}>
+                    Delete
+                </button>&nbsp;
+                <button id="done" onClick={ () => {handledone(todo.id);}}>
+                    Done
+                </button>
             </li>
           );
         })}
       </ul>
+      
     </div>
   );
 }
